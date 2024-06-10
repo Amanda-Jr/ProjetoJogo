561b0fc2a6f58a9f6529779dce715728250d460f
William Pereira <almeidapereirast@gmail.com>
2024-06-10 13:41:23 -0300

    Update Main.java

--- ProjetoJogo/src/jogoProjeto/Main.java
+++ ProjetoJogo/src/jogoProjeto/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Olá, mundo!");
-		Jokenpo jkp = new Jokenpo();
-		jkp.inicializarJogadas();
+		//Jokenpo jkp = new Jokenpo();
+		//jkp.inicializarJogadas();
 		//jkp.iniciarJogadores();
 		//jkp.jogar();
+		
+		JogoDaVelha j1 = new JogoDaVelha();
 	}
 
 }
